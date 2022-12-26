75f432e8d67b1aca8146ce1b62a996b2bcaabe58
Anzhalika Turlak <293852647@mail.ru>
2022-12-26 18:21:03 +0300

    add paginator

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 from products.models import ProductCategory, Product, Basket
-from users.models import User
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -13,16 +13,20 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id):
-    context = {'title': 'Store - Каталог', 'categories': ProductCategory.objects.all()}
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
 
-    if category_id:
-        category = ProductCategory.objects.get(id=category_id)
-        products = Product.objacts.filter(category=category)
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
 
-    else:
-        products = Product.objects.all()
+    context = {
+        'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
+    }
     return render(request, 'products/products.html', context)
+
 
 @login_required
 def basket_add(request, product_id):
@@ -37,6 +41,7 @@ def basket_add(request, product_id):
         basket.save()
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
 @login_required
 def basket_remove(request, basket_id):
