e3839e7bda14f8572bbe613b99ccffb9946fa09c
Anzhalika Turlak <293852647@mail.ru>
2023-01-03 11:10:19 +0300

    add django-allauth, registered app in gitHub, autorization users in github

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+
     'products',
     'users',
 
@@ -145,8 +152,27 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # EMAIL
-EMAIL_HOST = 'smtp.yandex.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'testprojectstest@yandex.ru'
-EMAIL_HOST_PASSWORD = 'zxcvB987654'
-EMAIL_USE_SSL = True
+# EMAIL_HOST = 'smtp.yandex.com'
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER = 'testprojectstest@yandex.ru'
+# EMAIL_HOST_PASSWORD = 'zxcvB987654'
+# EMAIL_USE_SSL = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# OAuth
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+            'SCOPE': [
+                'user',
+            ],
+        }
+    }
